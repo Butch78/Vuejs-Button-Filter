27a897228a38cfa5013430cc24b26d09aefe792d
Matthew <Matthewtyleraylward@hotmail.com>
2018-06-18 21:53:11 +1000

    Created Search Button and List Display

--- src/Components/HomePage.vue
+++ src/Components/HomePage.vue
@@ -12,18 +12,24 @@
         <div class="search">
           <el-input v-model="search"></el-input>
         </div>
+        <el-button type="primary" v-on:click="display = true">Search</el-button>
       </el-col>
     </el-row>
     <el-row>
-      <el-col :span="24">
-              <div :key="guide.name" v-for="guide in filteredGuide">
+      <div v-if="display === true">
+        <el-col :span="24">
+          <div :key="guide.name" v-for="guide in filteredGuide">
         <span>
           <router-link :to="`${guide.routerLink}`">
-             <el-button type="primary">{{guide.name}}</el-button>
+             <h3>{{guide.name}}</h3>
           </router-link>
+              <p>{{guide.info}}</p>
         </span> 
         </div>
       </el-col>
+
+      </div>
+
     </el-row>
       
   </el-main>
@@ -34,12 +40,13 @@ export default {
     data() {
       return {
         search: '',
+        display: false,
         guides: [
-            {name: 'Creating a Remedy Change', routerLink: 'Remedy/RemedyChange'},
-            {name: 'Creating a Remedy Incident', routerLink: 'Remedy/RemedyIncident'},
-            {name: 'AURDEV Access', routerLink: 'FolderAccess'},
-            {name: 'AURTEST Access', routerLink: 'Folder'},
-            {name: 'Requestings Hardware', routerLink: 'Remedy'}
+            {name: 'Creating a Remedy Change', info: 'How to create a simple Change in Remedy',  routerLink: 'Remedy/RemedyChange'},
+            {name: 'Creating a Remedy Incident', info:  'How to raise an Incident in Remedy', routerLink: 'Remedy/RemedyIncident'},
+            {name: 'AURDEV Access', info: 'Getting Access to AURDEV', routerLink: 'FolderAccess'},
+            {name: 'AURTEST Access', info: 'Getiting Access to AURTEST',  routerLink: 'Folder'},
+            {name: 'Requestings Hardware', info: 'Requesting Access to Certain Hardware', routerLink: 'Remedy'}
           ]
         } 
     },
